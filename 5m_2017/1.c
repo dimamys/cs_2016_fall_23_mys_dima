924137f16ef6eeebc7707e0ed7930c216cebfe35
Dima <dima.mys@mail.ru>
2017-07-04 20:30:47 +0300

    изменено:      5m_2017/1.c

--- 5m_2017/1.c
+++ 5m_2017/1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-float func(float x)
+float func(float b)
 
 {
 	return x*x*x;
@@ -11,32 +11,32 @@ float func(float x)
 void main()
 
 {
-	float s = 0.0, x = 0.0, l=0.0, h=0.0;
-	float F = 0.0, F1=0.0, F2=0.0;
+	float a = 0.0, b = 0.0, c=0.0, d=0.0;
+	float x = 0.0, y=0.0, z=0.0;
 	int i;
 
 	printf("Enter low boarder:\n");
-	scanf("%f", &l);
+	scanf("%f", &c);
   
 	printf("Enter high boarder:\n");
-	scanf("%f", &h);
+	scanf("%f", &d);
 
 	printf("Enter step:\n");
-	scanf("%f", &s);
+	scanf("%f", &a);
 
-	x = l;
+	b = c;
   
-	F1 = ((-3) * func(x) + 4 * (func(x+s)) - (func(x+2*s)))/(2*s);
-	F2 = (1 / (2 * s))*(-func(x) + func(x + 2 * s));
+	y = ((-3) * func(b) + 4 * (func(b+a)) - (func(b+2*a)))/(2*a);
+	z = (1 / (2 * a))*(-func(b) + func(b + 2 * a));
 
-	printf("%f\n", F1);
-	printf("%f\n", F2);
+	printf("%f\n", y);
+	printf("%f\n", z);
 
-	while  (x+2*s <= h)
+	while  (b+2*a <= d)
 	{
-		F = (1 / (2 * s))*(func(x) - 4 * (func(x + s))+3*(func(x+2*s))
-		printf("%f\n", F);
-		x = x + s;
+		x = (1 / (2 * a))*(func(b) - 4 * (func(b + a))+3*(func(b+2*a))
+		printf("%f\n", x);
+		b = b + a;
 
 	}
 }
